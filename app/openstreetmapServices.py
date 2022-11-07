2803b6ed5b384b9f13923eaeeefd26aec26dc0af
annagthb <anna2406k@hotmail.com>
2022-11-08 01:26:06 +0200

    AK: remove sensitive data from code, use a secure parameter store

--- app/openstreetmapServices.py
+++ app/openstreetmapServices.py
@@ -1,10 +1,12 @@
 class OpenstreetmapService:
+    ##TODO
     def __init__(self):
         self.url = "https://www.openstreetmap.org/"
-        self.authorisation={'Authorization':'Bearer ArJJPEmlED6mjSsFnVTETG59xBc5FoMCoohnoG_b33bK3pDQwLM4apVEy6xi3UJM','Content-Type':'application/json'}
+        self.authorisationHeader={'Authorization':'Bearer myEncryptedApiToken','Content-Type':'application/json'}
         
 
     def getBoundingBox(coordinates):
+        ##TODO
         apiRequest=self.api_url+"/api/0.6/map?bbox={left},{bottom},{right},{top}".format(left = coordinates["left"],bottom=coordinates["bottom"],right=coordinates["right"],top=coordinates["top"])
         response = requests.get(apiRequest)
         result=response.json()
